844d274973e7ff8b3c75ea42e9cc814f09cea937
youngrokkim2017 <patrick.yrkim@gmail.com>
2025-01-09 12:04:52 -0800

    Add model predictions

--- yolo_inference.py
+++ yolo_inference.py
@@ -2,4 +2,10 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8x')
 
-model.predict('input_videos/image.png', save=True)
+# model.predict('input_videos/image.png', save=True)
+
+result = model.predict('input_videos/input_video.mp4', save=True)
+print(result)
+print("boxes:")
+for box in result[0].boxes:
+    print(box)
