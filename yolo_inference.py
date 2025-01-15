2cae38cb1e6b9db79b7f2003832b82871e4e09db
youngrokkim2017 <patrick.yrkim@gmail.com>
2025-01-15 15:54:38 -0800

    Add yolov5 train

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,10 +1,14 @@
 from ultralytics import YOLO 
 
-model = YOLO('yolov8x')
+# yolov8 track players
+# model = YOLO('yolov8x')
+
+# yolov5 track ball
+model = YOLO('models/yolo5_last.pt')
 
 # model.predict('input_videos/image.png', save=True)
 
-result = model.predict('input_videos/input_video.mp4', save=True)
+result = model.predict('input_videos/input_video.mp4', conf=0.2, save=True)
 print(result)
 print("boxes:")
 for box in result[0].boxes:
