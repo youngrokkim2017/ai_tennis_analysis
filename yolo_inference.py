2552ebe68d83c5e9893f55498340eed2fe8ced37
youngrokkim2017 <patrick.yrkim@gmail.com>
2025-01-16 22:57:35 -0800

    Add ultralytics object tracking

--- yolo_inference.py
+++ yolo_inference.py
@@ -4,12 +4,15 @@ from ultralytics import YOLO
 # model = YOLO('yolov8x')
 
 # yolov5 track ball
-model = YOLO('models/yolo5_last.pt')
+# model = YOLO('models/yolo5_last.pt')
+model = YOLO('yolov8x')
 
 # model.predict('input_videos/image.png', save=True)
 
-result = model.predict('input_videos/input_video.mp4', conf=0.2, save=True)
-print(result)
-print("boxes:")
-for box in result[0].boxes:
-    print(box)
+result = model.track('input_videos/input_video.mp4', conf=0.2, save=True)
+
+# result = model.predict('input_videos/input_video.mp4', conf=0.2, save=True)
+# print(result)
+# print("boxes:")
+# for box in result[0].boxes:
+#     print(box)
