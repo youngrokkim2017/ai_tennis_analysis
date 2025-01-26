658adb2306d52fa8bfe69f29706fac3f4389bd48
youngrokkim2017 <patrick.yrkim@gmail.com>
2025-01-25 23:55:44 -0800

    Add player tracker

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from utils import (read_video, 
                    save_video)
+from trackers import PlayerTracker
 
 def main():
     # Read Video
     input_video_path = "input_videos/input_video.mp4"
     video_frames = read_video(input_video_path)
+
+    # Detecting players
+    player_tracker = PlayerTracker(model_path='yolov8x')
+    player_detections = player_tracker.detect_frames(video_frames)
 
     save_video(video_frames, "output_videos/output_video.avi")
 
